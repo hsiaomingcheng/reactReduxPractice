f7a6f5cca659de52d012fa897f8f9bdeefb01a55
Chris hsiao <chris.hsiao@igroup.tw>
2020-04-28 23:21:27 +0800

    新增 axios

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import axios from 'axios';
 import reducer from './reducers';
 import App from './components/App';
 
@@ -11,6 +12,25 @@ const store = createStore(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 /* eslint-enable */
+
+// axios({
+//     method: 'get',
+//     url: 'https://5ea5b3f02d86f00016b46164.mockapi.io/api/v1/users',
+// })
+//     .then((result) => { console.log(result.data); })
+//     .catch((err) => { console.error(err); });
+
+const instance = axios.create({
+    url: '/users',
+    baseURL: 'https://5ea5b3f02d86f00016b46164.mockapi.io/api/v1',
+    responseType: 'json',
+});
+
+instance.get('/users')
+    .then((response) => {
+        // eslint-disable-next-line no-console
+        console.log(response.data);
+    });
 
 render(
     <Provider store={store}>
